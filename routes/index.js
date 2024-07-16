cc0011469f0da0b74a07f6978fe04a4c2912fb91
frank <mainafrank400@gmail.com>
2024-07-16 14:31:53 +0300

    Solved the error on View

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) =>{
-  res.send('Hello World')
+  res.render('index')
+  // res.send('Hello World')
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const expressLayout = require('express-ejs-layouts')
 
 const indexRouter = require("./routes/index")
 
-app.set('view engne', 'ejs')
-app.set('view', __dirname + '/views')
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
 app.set('layout', 'layout/layout')
 app.use(expressLayout)
 app.use(express.static('public'))
 
 app.use('/', indexRouter)
 
-app.listen(process.env.PORT || 3001)
+app.listen(process.env.PORT || 3000)
